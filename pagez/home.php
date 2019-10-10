4b30515f483de97f55ec8f1a507f7e1bcb799224
Alexis <exerapidoXthorn@gmail.com>
2019-10-10 11:23:57 -0400

    Added register

--- pagez/home.php
+++ pagez/home.php
@@ -4,5 +4,5 @@
 	<hr class="my-4">
 	<p>We sell only the best and finest Duplicants, freshly printed at our warehouse! They are priced depending on
 	their abilities and apearance. We do ship to any city, country and even outer solar systems!</p>
-	<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	<a class="btn btn-primary btn-lg" href="index.php?page=shop" role="button">Learn more</a>
 </div>
