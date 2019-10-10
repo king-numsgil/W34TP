ff12253d309802978ef9ead68122a747a7fd6b87
Alexis <exerapidoXthorn@gmail.com>
2019-10-10 10:19:39 -0400

    include once pour le YEETAGE

--- index.php
+++ index.php
@@ -86,8 +86,7 @@ if (!isset($_GET["page"]) || empty($_GET["page"]) || !file_exists("pagez/" . $_G
 	<div class="row">
 		<div class="col-lg-8 col-md-10 mx-auto">
 			<?php
-			// Contenu principal pour la page!!
-			// <== YEETAGE HERE!
+			   include_once "pagez/" . $_GET["page"] . ".php";
 			?>
 		</div>
 	</div>
