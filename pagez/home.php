afe054742294ddc052998b0ffb2a9dad69801957
Daniel Grondin <king@numsgil.co>
2019-10-10 10:36:20 -0400

    Added home content (kinda serious)

--- pagez/home.php
+++ pagez/home.php
@@ -1,3 +1,8 @@
-<?php
-	echo "ur mom wus heer";
-?>
+<div class="jumbotron">
+	<h1 class="display-8">Welcome!</h1>
+	<p class="lead">Welcome to our humble Duplicant Store!</p>
+	<hr class="my-4">
+	<p>We sell only the best and finest Duplicants, freshly printed at our warehouse! They are priced depending on
+	their abilities and apearance. We do ship to any city, country and even outer solar systems!</p>
+	<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
