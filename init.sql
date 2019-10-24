b2059824f6fcd73e71a9ab9508f5583d968975cf
Daniel Grondin <king@numsgil.co>
2019-10-24 14:20:17 -0400

    Added duplicants and traits and stuff linking them...

--- init.sql
+++ init.sql
@@ -9,3 +9,71 @@ GRANT USAGE ON *.* TO 'dupe_store'@'localhost' REQUIRE NONE WITH
     MAX_USER_CONNECTIONS 0;
 CREATE DATABASE IF NOT EXISTS dupe_store;
 GRANT ALL PRIVILEGES ON dupe_store.* TO 'dupe_store'@'localhost';
+
+use dupe_store;
+SET default_storage_engine=InnoDB;
+
+create table users
+(
+    id         int auto_increment,
+    first_name varchar(128) not null,
+    last_name  varchar(128) not null,
+    email      varchar(256) not null,
+    password   varchar(128) not null,
+
+    constraint users_pk
+        primary key (id)
+);
+
+create unique index users_email_uindex
+    on users (email);
+
+create table duplicants
+(
+    id                int auto_increment,
+    name              varchar(32)   not null,
+    attr_construction int default 0 null,
+    attr_excavation   int default 0 null,
+    attr_machinery    int default 0 null,
+    attr_athletics    int default 0 null,
+    attr_science      int default 0 null,
+    attr_cuisine      int default 0 null,
+    attr_creativity   int default 0 null,
+    attr_strength     int default 0 null,
+    attr_medicine     int default 0 null,
+    attr_agriculture  int default 0 null,
+    attr_husbandry    int default 0 null,
+    picture           varchar(32)   not null,
+
+    constraint duplicants_pk
+        primary key (id)
+);
+
+create table traits
+(
+    id          int auto_increment,
+    title       varchar(64) not null,
+    is_positive boolean     not null,
+    description text        not null,
+
+    constraint traits_pk
+        primary key (id)
+);
+
+create unique index traits_title_uindex
+    on traits (title);
+
+create table duplicant_traits
+(
+	dupe_id int not null,
+	trait_id int not null,
+
+	constraint duplicant_traits_pk
+		primary key (dupe_id, trait_id),
+
+	constraint duplicant_traits___fk1
+		foreign key (dupe_id) references duplicants (id),
+
+	constraint duplicant_traits___fk2
+		foreign key (trait_id) references traits (id)
+);
