25a81eb8e16ad557ad6c13a30633a97cec1ff873
Alexis <exerapidoXthorn@gmail.com>
2019-10-24 14:21:29 -0400

    mysqli for shop & dupe capsule test

---
+++ pagez/shop.php
@@ -0,0 +1,29 @@
+<?php
+	/*$mysqli = new mysqli('localhost','dupe_store','ewHE4eNuPikdxIxP','dupe_store');
+
+	$query = "SELECT * FROM duplicants";
+
+	$result = $mysqli->query($query);
+?>
+<?=$row["id"]?>
+<?php
+	while($row = $result->fetch_assoc()){*/
+
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 col-md-10 mx-auto">
+			<div class="jumbotron">
+				<h1 class="display-8">Dupe #0</h1>
+				<p class="lead">His name is John</p>
+				<img src="pics_test/03.png">
+				<hr class="my-4">
+				<p>Interested?</p>
+				<a class="btn btn-primary btn-sm" href="index.php?page=details&id=" role="button">Click for more details</a>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+	//}
+?>
