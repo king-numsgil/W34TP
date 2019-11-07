db66f30ceb591da85a1b403fd78db037d9d05bc5
Alexis <exerapidoXthorn@gmail.com>
2019-11-07 15:44:35 -0500

    Restylize the style of stylish dupe details

--- pagez/details.php
+++ pagez/details.php
@@ -12,51 +12,13 @@
 		WHERE duplicants.id = " . $_GET["id"]
 	);
 ?>
-<div class="row">
-	<div class="col card-header">
+<div class="row justify-content-center">
+	<div class="col-lg-10 card-header">
 		<h2 align="center"><?=$row["name"]?></h2>
 	  	<div class="row">
-	    	<div class="col-lg-6">
-				<img class="card-img-top" src="<?= $row["picture"] ?>" alt="Dupe"/>
-				<div class="card-header" style="background-color: lightgrey">
-					<div class="row stats">
-						Agriculture : <?=$row["attr_agriculture"]?>
-					</div>
-					<div class="row stats">
-						Athletics : <?=$row["attr_athletics"]?>
-					</div>
-					<div class="row stats">
-						Construction : <?=$row["attr_construction"]?>
-					</div>
-					<div class="row stats">
-						Creativity : <?=$row["attr_creativity"]?>
-					</div>
-					<div class="row stats">
-						Cuisine : <?=$row["attr_cuisine"]?>
-					</div>
-					<div class="row stats">
-						Excavation : <?=$row["attr_excavation"]?>
-					</div>
-					<div class="row stats">
-						Husbandry : <?=$row["attr_husbandry"]?>
-					</div>
-					<div class="row stats">
-						Machinery : <?=$row["attr_machinery"]?>
-					</div>
-					<div class="row stats">
-						Medicine : <?=$row["attr_medicine"]?>
-					</div>
-					<div class="row stats">
-						Science : <?=$row["attr_science"]?>
-					</div>
-					<div class="row stats">
-						Strength : <?=$row["attr_strength"]?>
-					</div>
-				</div>
-	    	</div>
-	    	<div class="col-lg-6">
-	    		<div class="">
-	    			<table>
+	    	<div class="col-lg-8">
+				<img class="" src="<?= $row["picture"] ?>" alt="Dupe"/>
+					<table style="background-color: lightgrey; width:100%">
 		    			<?php while ($traits_row = $t_result->fetch_assoc()) {?>
 			    			<tr>		    					
 			    				<td>
@@ -73,9 +35,43 @@
 			    				</td>
 			    			</tr>
 		    			<?php } ?>
-
 	    			</table>
-	    		</div>
+				
+	    	</div>
+	    	<div class="col-lg-4">
+				<div class="row stats">
+					Agriculture : <?=$row["attr_agriculture"]?>
+				</div>
+				<div class="row stats">
+					Athletics : <?=$row["attr_athletics"]?>
+				</div>
+				<div class="row stats">
+					Construction : <?=$row["attr_construction"]?>
+				</div>
+				<div class="row stats">
+					Creativity : <?=$row["attr_creativity"]?>
+				</div>
+				<div class="row stats">
+					Cuisine : <?=$row["attr_cuisine"]?>
+				</div>
+				<div class="row stats">
+					Excavation : <?=$row["attr_excavation"]?>
+				</div>
+				<div class="row stats">
+					Husbandry : <?=$row["attr_husbandry"]?>
+				</div>
+				<div class="row stats">
+					Machinery : <?=$row["attr_machinery"]?>
+				</div>
+				<div class="row stats">
+					Medicine : <?=$row["attr_medicine"]?>
+				</div>
+				<div class="row stats">
+					Science : <?=$row["attr_science"]?>
+				</div>
+				<div class="row stats">
+					Strength : <?=$row["attr_strength"]?>
+				</div>
 	    	</div>    	
 	  	</div>
 	</div>
