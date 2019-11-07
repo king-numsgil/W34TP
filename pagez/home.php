df4820829f910e156c2c8ca17ba2b5bb50e1746c
Daniel Grondin <king@numsgil.co>
2019-11-07 13:31:35 -0500

    There was a booboo in Home...

--- pagez/home.php
+++ pagez/home.php
@@ -3,6 +3,6 @@
 	<p class="lead">Welcome to our humble Duplicant Store!</p>
 	<hr class="my-4">
 	<p>We sell only the best and finest Duplicants, freshly printed at our warehouse! They are priced depending on
-		their abilities and apearance. We do ship to any city, country and even outer solar systems!</p>
+		their abilities and appearance. We do ship to any city, country and even outer solar systems!</p>
 	<a class="btn btn-primary btn-lg" href="index.php?page=shop" role="button">Learn more</a>
 </div>
