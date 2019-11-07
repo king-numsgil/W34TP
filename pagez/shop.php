3aede9db58a9f997b91185f0b8b784c78a2df3e1
Daniel Grondin <king@numsgil.co>
2019-11-07 13:22:49 -0500

    Changed shop's layout from jumbotron to cards

--- pagez/shop.php
+++ pagez/shop.php
@@ -2,22 +2,20 @@
 $result = $db->query("SELECT * FROM duplicants");
 ?>
 <div class="row">
-	<?php
-	while ($row = $result->fetch_assoc()) {
-		?>
-		<div class="col-lg-4 col-md-10 mt-auto">
-			<div class="jumbotron">
-				<h1 class="display-8">Dupe #<?= $row["id"] ?></h1>
-				<p class="lead">Say hello to <?= $row["name"] ?></p>
-				<img src="<?= $row['picture'] ?>" alt="Dupe"/>
-				<hr class="my-4">
-				<p>Interested?</p>
-				<a class="btn btn-primary btn-sm" href="index.php?page=details&id=<?= $row["id"] ?>" role="button">
-					Click for more details
-				</a>
+	<?php while ($row = $result->fetch_assoc()) { ?>
+		<div class="col-lg-4 col-md-10 mt-auto" style="margin-bottom: 1rem">
+			<div class="card">
+				<div class="card-header">Dupe #<?= $row["id"] ?></div>
+				<img class="card-img-top" src="<?= $row['picture'] ?>" alt="Dupe"/>
+				<div class="card-body">
+					<p class="card-text" style="margin-top: 0">Say hello to <?= $row["name"] ?></p>
+				</div>
+				<div class="card-footer clearfix">
+					Interested? <a class="btn-sm btn-primary float-md-right" href="index.php?page=details&id=<?= $row["id"] ?>">
+						Details
+					</a>
+				</div>
 			</div>
 		</div>
-		<?php
-	}
-	?>
+	<?php } ?>
 </div>
