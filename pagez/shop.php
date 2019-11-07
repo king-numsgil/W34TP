3b9b74518407a6ff754a6b779d46f663bf3896af
Daniel Grondin <king@numsgil.co>
2019-11-07 08:49:24 -0500

    Fixed HTML warning (thanks JetBrains)

--- pagez/shop.php
+++ pagez/shop.php
@@ -29,7 +29,7 @@
 			<div class="jumbotron">
 				<h1 class="display-8">Dupe #<?=$row["id"]?></h1>
 				<p class="lead">Say hello to <?=$row["name"]?></p>
-				<img src="<?=$row['picture']?>">
+				<img src="<?=$row['picture']?>" alt="Dupe" />
 				<hr class="my-4">
 				<p>Interested?</p>
 				<a class="btn btn-primary btn-sm" href="index.php?page=details&id=<?=$row["id"]?>" role="button">Click for more details</a>
