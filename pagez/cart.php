417816ce99394c3d6bb5607c8140b11dd62d49c5
Daniel Grondin <king@numsgil.co>
2019-11-07 15:09:26 -0500

    Cart is now mostly working! Checkout is missing though...

--- pagez/cart.php
+++ pagez/cart.php
@@ -16,8 +16,14 @@ if (isset($_GET["action"])) {
 	}
 
 	$action = $_GET["action"];
-	if ($action === "add" && isset($_GET["id"]) && !empty($_GET["id"])) {
+	if ($action === "add" && isset($_GET["id"])) {
 		$_SESSION["cart"][] = $_GET["id"];
+		header("Location: index.php?page=cart");
+		die();
+	}
+	if ($action === "remove" && isset($_GET["i"])) {
+		unset($_SESSION["cart"][$_GET["i"]]);
+		$_SESSION["cart"] = array_values($_SESSION["cart"]);
 		header("Location: index.php?page=cart");
 		die();
 	}
@@ -38,7 +44,31 @@ if (isset($_GET["action"])) {
 				</div>
 			</div>
 		</div>
-	<?php } else {
-
-	}
+	<?php } else { ?>
+		<div class="row">
+			<?php for ($i = 0; $i < count($_SESSION["cart"]); $i++) {
+				$row = $db->query("SELECT * FROM duplicants WHERE id = {$_SESSION["cart"][$i]}")->fetch_assoc();
+				?>
+				<div class="col-lg-4 col-md-10 mt-auto" style="margin-bottom: 1rem">
+					<div class="card">
+						<div class="card-header">Dupe #<?= $row["id"] ?></div>
+						<img class="card-img-top" src="<?= $row['picture'] ?>" alt="Dupe"/>
+						<div class="card-body">
+							<p class="card-text" style="margin-top: 0">Say hello to <?= $row["name"] ?></p>
+						</div>
+						<div class="card-footer text-center">
+							<div class="btn-group" role="group" aria-label="Basic example">
+								<a class="btn btn-sm btn-danger" href="index.php?page=cart&action=remove&i=<?= $i ?>">
+									Remove
+								</a>
+								<a class="btn btn-sm btn-primary" href="index.php?page=details&id=<?= $row["id"] ?>">
+									Details
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+	<?php }
 } ?>
