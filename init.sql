56e040f48d8fb0c617ab10804d399fcf2401fe44
Alexis <exerapidoXthorn@gmail.com>
2019-11-14 09:34:34 -0500

    Fixed dupe names in script

--- init.sql
+++ init.sql
@@ -8,7 +8,7 @@ GRANT ALL PRIVILEGES ON dupe_store.* TO 'dupe_store'@'localhost';
 FLUSH PRIVILEGES;
 
 use dupe_store;
-SET default_storage_engine = InnoDB;
+SET default_storage_engine=InnoDB;
 
 create table users
 (
@@ -25,9 +25,9 @@ create table users
 create unique index users_email_uindex
     on users (email);
 
-insert into users(first_name, last_name, email, password)
-VALUES ('Daniel', 'Grondin', 'king@numsgil.co', 'salut'),
-       ('Alexis', 'Lepine', 'exerapidoxthorn@gmail.com', 'P@ssw0rd');
+insert into users(first_name, last_name, email, password) VALUES
+    ('Daniel', 'Grondin', 'king@numsgil.co', 'salut'),
+    ('Alexis', 'Lepine', 'exerapidoxthorn@gmail.com', 'P@ssw0rd');
 
 create table duplicants
 (
@@ -45,22 +45,20 @@ create table duplicants
     attr_agriculture  int default 0 null,
     attr_husbandry    int default 0 null,
     picture           varchar(32)   not null,
-    price             float         not null,
+	price             float         not null,
 
     constraint duplicants_pk
         primary key (id)
 );
 
-insert into duplicants(name, attr_construction, attr_excavation, attr_machinery, attr_athletics, attr_science,
-                       attr_cuisine, attr_creativity, attr_strength, attr_medicine, attr_agriculture, attr_husbandry,
-                       picture, price)
-VALUES ('Bobby', 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/01.png', 10),
-       ('Gerald', 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/02.png', 14),
-       ('Hannah', 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/03.png', 19),
-       ('Judy', 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'pics_test/04.png', 47),
-       ('Troy', 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 'pics_test/05.png', 2),
-       ('Yeep', 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 'pics_test/06.png', 25),
-       ('Leia', 0, 7, 0, 0, 0, 0, 7, 0, 0, 0, 0, 'pics_test/07.png', 68);
+insert into duplicants(name, attr_construction, attr_excavation, attr_machinery, attr_athletics, attr_science, attr_cuisine, attr_creativity, attr_strength, attr_medicine, attr_agriculture, attr_husbandry, picture, price) VALUES
+    ('Hannah', 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/01.png', 10),
+    ('Judy', 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/02.png', 14),
+    ('Bobby', 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 'pics_test/03.png', 19),
+    ('Gerald', 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 'pics_test/04.png', 47),
+    ('Troy', 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 'pics_test/05.png', 2),
+    ('Yeep', 0, 0, 3, 0, 0, 0, 3, 0, 0, 0, 0, 'pics_test/06.png', 25),
+    ('Leia',0,7,0,0,0,0,7,0,0,0,0,'pics_test/07.png',68);
 
 create table traits
 (
@@ -76,43 +74,40 @@ create table traits
 create unique index traits_title_uindex
     on traits (title);
 
-insert into traits(title, is_positive, description)
-VALUES ('Bottomless Stomach', false, 'Decreased Callories'),
-       ('Night Owl', true, 'Gains nighttime Attribute bonuses'),
-       ('Gourmet', true, 'Increased Cuisine<br />Decreased Food Morale Bonus'),
-       ('Loud Sleeper', false, 'Snores loudly'),
-       ('Diver\'s Lung', true, 'Decreased Air Consumption Rate'),
-       ('Allergies', false, 'Allergic reaction to Floral Scent'),
-       ('Early Bird', true, 'Gains morning Attribute bonuses'),
-       ('Flatulent', false, 'Farts frequently'),
-       ('Yokel', false, 'Cannot do <b>Researching Errands</b>'),
-       ('Biohazardous', false, 'Decreased <b>Germ Resistance</b>'),
-       ('Quick Learner', true, 'Increased <b>Science</b>'),
-       ('Caregiver', true, 'Increased <b>Medicine</b>'),
-       ('Grease Monkey', true, 'Increased <b>Machinery</b>'),
-       ('Narcoleptic', false, 'Falls asleep periodically'),
-       ('Iron Gut', true, 'Immune to <b>Food Poisoning</b>'),
-       ('Gastrophobia', false, 'Cannot do <b>Cooking Errands</b>');
+insert into traits(title, is_positive, description) VALUES
+    ('Bottomless Stomach', false, 'Decreased Callories'),
+    ('Night Owl', true, 'Gains nighttime Attribute bonuses'),
+    ('Gourmet', true, 'Increased Cuisine<br />Decreased Food Morale Bonus'),
+    ('Loud Sleeper', false, 'Snores loudly'),
+    ('Diver\'s Lung', true, 'Decreased Air Consumption Rate'),
+    ('Allergies', false, 'Allergic reaction to Floral Scent'),
+    ('Early Bird', true, 'Gains morning Attribute bonuses'),
+    ('Flatulent', false, 'Farts frequently'),
+    ('Yokel', false, 'Cannot do <b>Researching Errands</b>'),
+    ('Biohazardous', false, 'Decreased <b>Germ Resistance</b>'),
+    ('Quick Learner', true, 'Increased <b>Science</b>'),
+    ('Caregiver', true, 'Increased <b>Medicine</b>'),
+    ('Grease Monkey', true, 'Increased <b>Machinery</b>'),
+    ('Narcoleptic', false, 'Falls asleep periodically'),
+    ('Iron Gut', true, 'Immune to <b>Food Poisoning</b>'),
+    ('Gastrophobia', false, 'Cannot do <b>Cooking Errands</b>');
 
 create table duplicant_traits
 (
-    dupe_id  int not null,
-    trait_id int not null,
+	dupe_id int not null,
+	trait_id int not null,
 
-    constraint duplicant_traits_pk
-        primary key (dupe_id, trait_id),
+	constraint duplicant_traits_pk
+		primary key (dupe_id, trait_id),
 
-    constraint duplicant_traits___fk1
-        foreign key (dupe_id) references duplicants (id),
+	constraint duplicant_traits___fk1
+		foreign key (dupe_id) references duplicants (id),
 
-    constraint duplicant_traits___fk2
-        foreign key (trait_id) references traits (id)
+	constraint duplicant_traits___fk2
+		foreign key (trait_id) references traits (id)
 );
 
-insert into duplicant_traits(dupe_id, trait_id)
-VALUES (1, 1),
-       (1, 2),
-       (2, 3),
-       (2, 4),
-       (3, 5),
-       (3, 6);
+insert into duplicant_traits(dupe_id, trait_id) VALUES
+    (1, 1), (1, 2),
+    (2, 3), (2, 4),
+    (3, 5), (3, 6);
