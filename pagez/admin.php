f81e75e0f24351923441234c0d237f701addc8f8
Daniel Grondin <king@numsgil.co>
2019-11-14 14:18:49 -0500

    Now there's a master page in a master page! Admin got bling!

--- pagez/admin.php
+++ pagez/admin.php
@@ -10,21 +10,38 @@ if (!$user["is_admin"]) {
 }
 
 if (!isset($_GET["action"])) { ?>
-	Menu
+	<div class="row justify-content-center" style="margin-bottom: 2rem">
+		<div class="col-5 text-center">
+			<h4 class="display-4">Admin Actions</h4>
+		</div>
+	</div>
+	<div class="row justify-content-between">
+		<div class="col-3">
+			<a class="btn btn-lg btn-block btn-warning" href="index.php?page=admin&action=modify">
+				Modify
+			</a>
+		</div>
+
+		<div class="col-3">
+			<a class="btn btn-lg btn-block btn-success" href="index.php?page=admin&action=create">
+				Create
+			</a>
+		</div>
+
+		<div class="col-3">
+			<a class="btn btn-lg btn-block btn-danger" href="index.php?page=admin&action=delete">
+				Delete
+			</a>
+		</div>
+	</div>
 <?php } else {
 	$action = $_GET["action"];
 
-	if ($action === "modify") {?>
-
-		Modify
-
-	<?php } elseif ($action === "create") { ?>
-
-		Create
-
-	<?php } elseif ($action === "delete") { ?>
-
-		Delete
-
-	<?php }
+	if ($action === "modify") {
+		include_once "admin/modify.php";
+	} elseif ($action === "create") {
+		include_once "admin/create.php";
+	} elseif ($action === "delete") {
+		include_once "admin/delete.php";
+	}
 }
