54aceeed6515911549f079c72fd2b91926ca2d4a
Alexis <exerapidoXthorn@gmail.com>
2019-11-14 14:49:38 -0500

    search bar

--- pagez/shop.php
+++ pagez/shop.php
@@ -6,9 +6,46 @@ if (!isset($_GET["offset"]) || !isset($_GET["limit"])) {
 	$offset = $_GET["offset"]; $limit = $_GET["limit"];
 	$count = $db->query("SELECT COUNT(id) FROM duplicants")->fetch_assoc()["COUNT(id)"];
 	$result = $db->query("SELECT * FROM duplicants LIMIT $limit OFFSET $offset");
+
+	$traits_good = $db->query("SELECT title FROM traits WHERE is_positive = true");
+	$traits_bad = $db->query("SELECT title FROM traits WHERE is_positive = false");
+
+
 ?>
+<button class="btn btn-primary mb-3" type="button" data-toggle="collapse" data-target="#filter" aria-expanded="false" aria-controls="filter">
+	Traits Filter
+</button>
+	<div class="collapse bg-light" style="padding:12px" id="filter">
+		<form action="#" method="post" class="row">
+			<div class="col-12 text-muted mb-2">
+				Pick what you want in your dupe : a good trait, a bad trait, or both! We'll sort the duplicants with those traits for you.
+			</div>
+			<div class="col-md-6">
+				<select name="trait_good" class="browser-default custom-select custom-select-lg mb-3">
+					<option class="text-muted">-- Good Traits --</option>
+					<?php while($good_row = $traits_good->fetch_assoc()){ ?>
+						<option value="<?= $good_row["title"]?>"><?= $good_row["title"]?></option>
+					<?php } ?>
+				</select>
+			</div>
+			<div class="col-md-6">
+				<select name="trait_bad" class="browser-default custom-select custom-select-lg mb-3">
+					<option class="text-muted">-- Bad Traits --</option>
+					<?php while($bad_row = $traits_bad->fetch_assoc()){ ?>
+						<option value="<?= $bad_row["title"]?>"><?= $bad_row["title"]?></option>
+					<?php } ?>
+				</select>
+			</div>
+			<div class="col-12">
+				<button type="submit" class="btn btn-success btn-block">
+					Search
+				</button>
+			</div>
+		</form>
+	</div>
+	
 	<div class="row align-items-start">
-		<?php while ($row = $result->fetch_assoc()) { ?>
+		<?php while ($row = $result->fetch_assoc()){ ?>
 			<div class="col-lg-4 col-md-10" style="margin-bottom: 1rem">
 				<div class="card">
 					<div class="card-header">Dupe #<?= $row["id"] ?></div>
@@ -18,7 +55,7 @@ if (!isset($_GET["offset"]) || !isset($_GET["limit"])) {
 					</div>
 					<ul class="list-group list-group-flush">
 						<?php
-							$traits = $db->query("
+							$dupe_traits = $db->query("
 								SELECT title, is_positive FROM traits 
 								INNER JOIN duplicant_traits 
 								ON duplicant_traits.trait_id = traits.id
@@ -26,7 +63,7 @@ if (!isset($_GET["offset"]) || !isset($_GET["limit"])) {
 								ON duplicants.id = duplicant_traits.dupe_id
 								WHERE duplicants.id = {$row["id"]}
 								");
-							while($t_row = $traits->fetch_assoc()){	
+							while($t_row = $dupe_traits->fetch_assoc()){	
 								if($t_row["is_positive"] == true){
 								?>
 							<li class="list-group-item" style="color:green"><?= $t_row["title"]?></li>
@@ -57,6 +94,7 @@ if (!isset($_GET["offset"]) || !isset($_GET["limit"])) {
 				</div>
 			</div>
 		<?php } ?>
+	
 	</div>
 	<div class="row">
 		<div class="col text-left">
