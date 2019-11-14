7dd6146c2c85139ce29403eb65310b9058160b0e
Alexis <exerapidoXthorn@gmail.com>
2019-11-14 13:30:48 -0500

    Made some cleaning in home page

--- pagez/home.php
+++ pagez/home.php
@@ -1,8 +1,8 @@
 <div class="jumbotron">
-	<h1 class="display-8">Welcome!</h1>
+	<h1 class="display-8">Customer detected!</h1>
 	<p class="lead">Welcome to our humble Duplicant Store!</p>
 	<hr class="my-4">
 	<p>We sell only the best and finest Duplicants, freshly printed at our warehouse! They are priced depending on
 		their abilities and appearance. We do ship to any city, country and even outer solar systems!</p>
-	<a class="btn btn-primary btn-lg" href="index.php?page=shop" role="button">Learn more</a>
+	<a class="btn btn-primary btn-lg" href="index.php?page=shop" role="button">Click here to check them out!</a>
 </div>
