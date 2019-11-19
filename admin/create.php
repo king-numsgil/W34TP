8d4fcbeb0688b711337000ed14f2a58e6173c0d2
Daniel Grondin <king@numsgil.co>
2019-11-19 14:58:34 -0500

    Added form to create a Duplicant! Trait assignment is still missing

--- admin/create.php
+++ admin/create.php
@@ -1,4 +1,146 @@
 <?php
+if (!isset($_GET["mode"]) || empty($_GET["mode"])) {
+	header("Location: index.php?page=admin&action=create&mode=dupe");
+	die();
+}
+
+$mode = $_GET["mode"];
 ?>
 
 <h1>Create</h1>
+<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a class="nav-link <?= $mode === "dupe" ? "active" : "" ?>" href="index.php?page=admin&action=create&mode=dupe">
+			Duplicant
+		</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link <?= $mode === "trait" ? "active" : "" ?>" href="index.php?page=admin&action=create&mode=trait">
+			Trait
+		</a>
+	</li>
+</ul>
+
+<?php if ($mode === "dupe") {
+	if (isset($_GET["apply"])) {
+		$db->query("INSERT INTO duplicants(name, attr_construction, attr_excavation, attr_machinery, " .
+			"attr_athletics, attr_science, attr_cuisine, attr_creativity, attr_strength, attr_medicine, " .
+			"attr_agriculture, attr_husbandry, picture, price) VALUES (" .
+			"'{$_POST["name"]}', " .
+			"'{$_POST["construction"]}', " .
+			"'{$_POST["excavation"]}', " .
+			"'{$_POST["machinery"]}', " .
+			"'{$_POST["athletics"]}', " .
+			"'{$_POST["science"]}', " .
+			"'{$_POST["cuisine"]}', " .
+			"'{$_POST["creativity"]}', " .
+			"'{$_POST["strength"]}', " .
+			"'{$_POST["medicine"]}', " .
+			"'{$_POST["agriculture"]}', " .
+			"'{$_POST["husbandry"]}', " .
+			"'{$_POST["pic"]}', " .
+			"'{$_POST["price"]}'" .
+			")");
+		header("Location: index.php?page=admin&action=create&mode=dupe");
+		die();
+	} else { ?>
+
+	<form class="mt-sm-4" method="post" action="index.php?page=admin&action=create&mode=dupe&apply">
+		<div class="form-group row">
+			<label for="dupe_name" class="col-sm-2 col-form-label">Name :</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="dupe_name" name="name" required maxlength="32"/>
+			</div>
+		</div>
+		<div class="form-group row">
+			<label for="dupe_pic" class="col-sm-2 col-form-label">Picture :</label>
+			<div class="col-sm-10">
+				<input type="text" class="form-control" id="dupe_pic" name="pic" required maxlength="32"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_agriculture" class="col-sm-2 col-form-label">Agriculture :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_agriculture" name="agriculture" required value="0"/>
+			</div>
+
+			<label for="dupe_athletics" class="col-sm-2 col-form-label">Athletics :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_athletics" name="athletics" required value="0"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_construction" class="col-sm-2 col-form-label">Construction :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_construction" name="construction" required value="0"/>
+			</div>
+
+			<label for="dupe_creativity" class="col-sm-2 col-form-label">Creativity :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_creativity" name="creativity" required value="0"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_cuisine" class="col-sm-2 col-form-label">Cuisine :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_cuisine" name="cuisine" required value="0"/>
+			</div>
+
+			<label for="dupe_excavation" class="col-sm-2 col-form-label">Excavation :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_excavation" name="excavation" required value="0"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_husbandry" class="col-sm-2 col-form-label">Husbandry :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_husbandry" name="husbandry" required value="0"/>
+			</div>
+
+			<label for="dupe_machinery" class="col-sm-2 col-form-label">Machinery :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_machinery" name="machinery" required value="0"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_medicine" class="col-sm-2 col-form-label">Medicine :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_medicine" name="medicine" required value="0"/>
+			</div>
+
+			<label for="dupe_science" class="col-sm-2 col-form-label">Science :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_science" name="science" required value="0"/>
+			</div>
+		</div>
+
+		<div class="form-group row">
+			<label for="dupe_strength" class="col-sm-2 col-form-label">Strength :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_strength" name="strength" required value="0"/>
+			</div>
+
+			<label for="dupe_price" class="col-sm-2 col-form-label">Price :</label>
+			<div class="col-sm-4">
+				<input type="number" class="form-control" id="dupe_price" name="price" required value="0.0"/>
+			</div>
+		</div>
+
+		<div class="form-group row justify-content-center">
+			<div class="col-sm-9">
+				<button type="submit" class="btn btn-block btn-warning">Save</button>
+			</div>
+		</div>
+	</form>
+
+<?php } } else { ?>
+
+<?php } ?>
+
+<hr />
+<a class="btn btn-block btn-success" href="index.php?page=admin">Back to Administration page</a>
