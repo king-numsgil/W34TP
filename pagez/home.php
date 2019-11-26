772f0dc9ac164b2db570806f43df192edd0c1235
Daniel Grondin <king@numsgil.co>
2019-11-26 15:15:05 -0500

    Revert "Sshhh...."
    This reverts commit 61fac7be93c5d1f04035efe144bb9d1849c68955.

--- pagez/home.php
+++ pagez/home.php
@@ -5,6 +5,4 @@
 	<p>We sell only the best and finest Duplicants, freshly printed at our warehouse! They are priced depending on
 		their abilities and appearance. We do ship to any city, country and even outer solar systems!</p>
 	<a class="btn btn-primary btn-lg" href="index.php?page=shop" role="button">Click here to check them out!</a>
-
-	<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/IfyjWLQMeF6kbG2r0z" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/IfyjWLQMeF6kbG2r0z">via GIPHY</a></p>
 </div>
