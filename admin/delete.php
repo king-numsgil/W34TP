39c78770f3a9ee71fe9befa653652e1b27673746
Daniel Grondin <king@numsgil.co>
2019-11-27 09:14:27 -0500

    Reformatted delete

--- admin/delete.php
+++ admin/delete.php
@@ -1,38 +1,37 @@
 <?php
-	if (!isset($_GET["mode"]) || empty($_GET["mode"])) {
+if (!isset($_GET["mode"]) || empty($_GET["mode"])) {
 	header("Location: index.php?page=admin&action=modify&mode=dupe");
 	die();
-	}
+}
 
-	$mode = $_GET["mode"];
+$mode = $_GET["mode"];
 
-	if ($mode === "dupe"){
-		$db->query("DELETE FROM duplicant_traits WHERE dupe_id = ".$_GET["id"]);
-		$db->query("DELETE FROM duplicants WHERE id = ".$_GET["id"]);
-?>
+if ($mode === "dupe") {
+	$db->query("DELETE FROM duplicant_traits WHERE dupe_id = " . $_GET["id"]);
+	$db->query("DELETE FROM duplicants WHERE id = " . $_GET["id"]);
+	?>
+	<script>
+		alert("Duplicant succesfully deleted. Goodbye, <?= $_GET['name']?>!");
+		window.location = "index.php?page=admin&action=modify&mode=dupe";
+	</script>
+	<?php
+} else if ($mode === "trait") {
+	$trait_found = $db->query("SELECT count(trait_id) FROM duplicant_traits WHERE trait_id = " . $_GET["id"])->fetch_assoc()["count(trait_id)"];
+
+	if ($trait_found < 1) {
+		$db->query("DELETE FROM traits WHERE id = " . $_GET["id"]);
+		?>
 		<script>
-			alert("Duplicant succesfully deleted. Goodbye, <?= $_GET['name']?>!");
-			window.location="index.php?page=admin&action=modify&mode=dupe";
+			alert("This trait was succesfully deleted!");
+			window.location = "index.php?page=admin&action=modify&mode=trait";
 		</script>
-<?php
+		<?php
+	} else { ?>
+		<script>
+			alert("Cannot delete this trait, unless you are heartless and want duplicants to lose a piece of their personnality... Anyway, no!");
+			window.location = "index.php?page=admin&action=modify&mode=trait";
+		</script>
+		<?php
 	}
-	else if($mode === "trait"){
-		$trait_found = $db->query("SELECT count(trait_id) FROM duplicant_traits WHERE trait_id = ".$_GET["id"])->fetch_assoc()["count(trait_id)"];
-
-		if($trait_found < 1) {
-			$db->query("DELETE FROM traits WHERE id = ".$_GET["id"]);
-?>
-			<script>
-				alert("This trait was succesfully deleted!");
-				window.location="index.php?page=admin&action=modify&mode=trait";
-			</script>
-<?php
-		} else { ?>
-			<script>
-				alert("Cannot delete this trait, unless you are heartless and want duplicants to lose a piece of their personnality... Anyway, no!");
-				window.location="index.php?page=admin&action=modify&mode=trait";
-			</script>
-<?php
-		}
-	}
+}
 ?>
